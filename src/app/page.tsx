a3bcbf7884b14f9d9a92807f53bd2a1a397b860b
Lukáš Peťko <luke.petko@gmail.com>
2024-02-01 21:47:33 +0100

    adding pwa support

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,12 +10,6 @@ export default async function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Next js</h1>
       <UserButton />
-      {rows.map((row) => (
-        <>
-          <div>{row.id}</div>
-          <div>{row.name}</div>
-        </>
-      ))}
     </main>
   );
 }
