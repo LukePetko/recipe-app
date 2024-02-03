4fd818ad5af31b939c9bd5bd012a8b410e6c9840
Lukáš Peťko <luke.petko@gmail.com>
2024-02-03 17:06:18 +0100

    centering signin and signup screens

--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -2,7 +2,7 @@ import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <div className="flex justify-center items-center">
+    <div className="h-full flex justify-center items-center">
       <SignUp />
     </div>
   );
